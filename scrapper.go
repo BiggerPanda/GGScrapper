62ee32db6ba6f2053f85e92095bab2df959131a0
Jakub Michalski <robertmenel@gmail.com>
2023-06-21 19:30:51 +0200

    saving in struct

--- scrapper.go
+++ scrapper.go
@@ -18,13 +18,14 @@ func main() {
 	baseURL := "https://gg.deals/game/satisfactory/"
 
 	fmt.Println("Hello World")
+	games := []Game{}
 	c := colly.NewCollector(colly.CacheDir("./ggdeals_cache"))
-	initCollectro(c)
+	initCollectro(c, &games)
 	c.Visit(baseURL)
 
 }
 
-func initCollectro(c *colly.Collector) {
+func initCollectro(c *colly.Collector, games *[]Game) {
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting: ", r.URL)
@@ -39,15 +40,17 @@ func initCollectro(c *colly.Collector) {
 	})
 
 	c.OnHTML("div.offer-section.with-filters", func(e *colly.HTMLElement) {
-
+		i := 1
 		e.ForEach("div.similar-deals-container.items-with-top-border-desktop", func(_ int, el *colly.HTMLElement) {
-			fmt.Println("Found Name: ", el.ChildAttr("div.relative.hoverable-box.d-flex.flex-wrap.flex-align-center.game-item.cta-full.item.game-deals-item.game-list-item.keep-unmarked-container", "data-game-name"))
-			fmt.Println("Found Shop: ", el.ChildAttr("div.relative.hoverable-box.d-flex.flex-wrap.flex-align-center.game-item.cta-full.item.game-deals-item.game-list-item.keep-unmarked-container", "data-shop-name"))
-			fmt.Println("Found price: ", el.ChildText(".price-inner.game-price-current"))
+			game := Game{}
+			game.name = el.ChildAttr("div.relative.hoverable-box.d-flex.flex-wrap.flex-align-center.game-item.cta-full.item.game-deals-item.game-list-item.keep-unmarked-container", "data-game-name")
+			game.shop = el.ChildAttr("div.relative.hoverable-box.d-flex.flex-wrap.flex-align-center.game-item.cta-full.item.game-deals-item.game-list-item.keep-unmarked-container", "data-shop-name")
+			game.price = el.ChildText(".price-inner.game-price-current")
 			link := el.ChildAttr(".full-link", "href")
-			fmt.Println("Found link: ", el.Request.AbsoluteURL(link))
-			fmt.Println(" ")
-
+			game.link = el.Request.AbsoluteURL(link)
+			*games = append(*games, game)
+			fmt.Println("Game: ", i)
+			i++
 		})
 	})
 
