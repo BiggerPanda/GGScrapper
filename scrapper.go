9636f9f70a3928bed05a9d0676b20b00e2f88039
Jakub Michalski <robertmenel@gmail.com>
2023-06-21 18:07:39 +0200

    few changes

--- scrapper.go
+++ scrapper.go
@@ -3,15 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
+	"path"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
+	baseURL := "https://gg.deals/game/satisfactory/"
+
 	fmt.Println("Hello World")
 	c := colly.NewCollector()
 	initCollectro(c)
-	c.Visit("https://gg.deals/game/satisfactory/")
+	c.Visit(baseURL)
 
 }
 
@@ -29,7 +33,11 @@ func initCollectro(c *colly.Collector) {
 		fmt.Println("Page visited: ", r.Request.URL)
 	})
 
-	c.OnHTML(".d-flex.header-game-prices-content.active", func(e *colly.HTMLElement) {
+	c.OnHTML(".similar-deals-container.items-with-top-border-desktop", func(e *colly.HTMLElement) {
+		u, _ := url.Parse("https://gg.deals")
+		u.Path = path.Join(u.Path, e.ChildAttr("a", "href"))
+		c.Visit(u.String())
+		fmt.Println("Found something: ", u)
 		fmt.Println("Found something: ", e.ChildText(".price"))
 	})
 
